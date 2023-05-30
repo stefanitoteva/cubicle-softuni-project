42c1b592b2e44fb571c3871284451935547fe0a1
Stefani Toteva <stefani.toteva13@gmail.com>
2023-05-30 21:43:36 +0300

    Configurate Express to project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("test");
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Test');
+})
+
+app.listen(PORT, () => console.log(`The server is running on port ${PORT}...`));
