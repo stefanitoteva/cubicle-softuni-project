377df53dec8d8d69c484a62560fda3778e5de1c8
Stefani Toteva <stefani.toteva13@gmail.com>
2023-05-31 09:06:40 +0300

    Add about action to home controller

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -2,6 +2,10 @@ const router = require('express').Router();
 
 router.get('/', (req, res) => {
     res.render('index');
+});
+
+router.get('/about', (req, res) => {
+    res.render('about');
 })
 
 
