3973f20dd0e64ee43c4c7af096e65e4851e71f3f
Stefani Toteva <stefani.toteva13@gmail.com>
2023-06-05 21:39:44 +0300

    Extract all cubes from DB

--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -1,29 +1,22 @@
-const uniqid = require('uniqid');
 const Cube = require('../models/Cube');
-const cubes = [];
 
 exports.getAll = async (search, from, to) => {
+    let result = await Cube.find().lean();
 
-    return await Cube.find().lean()
+    if (search) {
+        result = result.filter(cube => cube.name.toLowerCase().includes(search.toLowerCase()));
+    }
+
+    if (from) {
+        result = result.filter(cube => cube.difficultyLevel >= Number(from));
+    }
+
+    if (to) {
+        result = result.filter(cube => cube.difficultyLevel <= Number(to));
+    }
+
+    return result
 }
-
-// exports.getAll = (search, from, to ) => {
-//     let result = cubes.slice();
-
-//     if(search) {
-//         result = result.filter(cube => cube.name.toLowerCase().includes(search.toLowerCase()));
-//     }
-
-//     if(from) {
-//         result = result.filter(cube => cube.difficultyLevel >= Number(from));
-//     }
-
-//     if(to) {
-//         result = result.filter(cube => cube.difficultyLevel <= Number(to));
-//     }
-
-//     return result
-// }
 
 exports.getOne = (cubeId) => Cube.findById(cubeId);
 
