0dcb9033c3772ee6b67aac362326c54f3284aca9
Stefani Toteva <stefani.toteva13@gmail.com>
2023-06-05 18:43:06 +0300

    Show cubes on homepage via Mongoose
    Update functionality to extract cubes from DB

--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -2,23 +2,28 @@ const uniqid = require('uniqid');
 const Cube = require('../models/Cube');
 const cubes = [];
 
-exports.getAll = (search, from, to ) => {
-    let result = cubes.slice();
+exports.getAll = async (search, from, to) => {
 
-    if(search) {
-        result = result.filter(cube => cube.name.toLowerCase().includes(search.toLowerCase()));
-    }
-
-    if(from) {
-        result = result.filter(cube => cube.difficultyLevel >= Number(from));
-    }
-
-    if(to) {
-        result = result.filter(cube => cube.difficultyLevel <= Number(to));
-    }
-
-    return result
+    return await Cube.find().lean()
 }
+
+// exports.getAll = (search, from, to ) => {
+//     let result = cubes.slice();
+
+//     if(search) {
+//         result = result.filter(cube => cube.name.toLowerCase().includes(search.toLowerCase()));
+//     }
+
+//     if(from) {
+//         result = result.filter(cube => cube.difficultyLevel >= Number(from));
+//     }
+
+//     if(to) {
+//         result = result.filter(cube => cube.difficultyLevel <= Number(to));
+//     }
+
+//     return result
+// }
 
 exports.getOne = (cubeId) => cubes.find(x => x.id == cubeId);
 
