e2a6cdb5c240aafabe48549866a3d59d346e798a
Stefani Toteva <stefani.toteva13@gmail.com>
2023-06-08 22:04:33 +0300

    Save user with hashed password

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     username: String,
@@ -10,7 +11,13 @@ userSchema.virtual('repeatPassword')
         if(value !== this.password) {
             throw new mongoose.MongooseError('Passwords don\'t match!');
         }
-    })
+});
+
+userSchema.pre('save', async function(){
+    const hash = await bcrypt.hash(this.password, 10);
+
+    this.password = hash;
+})
 
 const User = mongoose.model('User', userSchema);
 
