006dd02cf7877e8138571eecf5ba509b5f5f6b00
Stefani Toteva <stefani.toteva13@gmail.com>
2023-06-11 13:36:36 +0300

    User logout

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -25,6 +25,11 @@ router.post('/login', async (req, res) => {
     res.cookie('token', token, { httpOnly: true });
 
     res.redirect('/');
+});
+
+router.get('/logout', (req, res) => {
+    res.clearCookie('token');
+    res.redirect('/');
 })
 
 
